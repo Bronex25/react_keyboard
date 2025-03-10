5d766110b0ef3dd60005ee717375410df896c118
Oleksandr Plaksin <157797075+Bronex25@users.noreply.github.com>
2025-03-11 01:52:28 +0700

    Update src/App.tsx
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ export const App: React.FC = () => {
     return () => {
       document.removeEventListener('keydown', keyPressHandler);
     };
-  });
+  }, []);
 
   return (
     <div className="App">
